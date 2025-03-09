0119cd672f250f73547911527d5b2d7d9a4c4e63
Scrappyz <ghostmic3000@gmail.com>
2025-03-09 14:11:20 +0800

    Added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button variant='primary outline-primary'>Button</Button>
+    </>
+  )
+}
+
+export default App
