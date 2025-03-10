4154db3f398dad1905d44639a76c7843a928b342
Scrappyz <ghostmic3000@gmail.com>
2025-03-10 14:50:33 +0800

    Added price to products

--- frontend/src/user/components/ProductCard/ProductCard.jsx
+++ frontend/src/user/components/ProductCard/ProductCard.jsx
@@ -4,11 +4,19 @@ import './ProductCard.css';
 
 function ProductCard() {
     return (
-		<div className='card rounded-3 shadow'>
+		<div className='card p-0 rounded-3 shadow' style={{maxWidth: "16rem"}}>
 			<img src='hotdog.jpg' />
 			<div className='card-body'>
-				<h4 className='card-title'>Hotdog</h4>
-				<p className='card-text'>Good old hotdog</p>
+				<div>
+					<h5 className='card-title'>Hotdog</h5>
+				</div>
+				<div>
+					<p className='card-text'>Good old hotdog</p>
+				</div>
+				<div className='d-flex justify-content-between mt-4' style={{fontSize: "16px"}}>
+					<strong className='card-text'>Price:</strong>
+					<p className='card-text' style={{color: "green"}}>PHP 10</p>
+				</div>
 			</div>
 		</div>
 	)
