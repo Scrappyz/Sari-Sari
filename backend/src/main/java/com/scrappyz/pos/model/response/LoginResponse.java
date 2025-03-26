870c9d6e39da9d4ccea498c5d0d05e20f88d514a
Scrappyz <ghostmic3000@gmail.com>
2025-03-26 20:09:21 +0800

    Put 'response' inside 'model'

---
+++ backend/src/main/java/com/scrappyz/pos/model/response/LoginResponse.java
@@ -0,0 +1,25 @@
+package com.scrappyz.pos.model.response;
+
+public class LoginResponse {
+  private String message;
+  private String status;
+  private String other;
+  public String getMessage() {
+    return message;
+  }
+  public void setMessage(String message) {
+    this.message = message;
+  }
+  public String getStatus() {
+    return status;
+  }
+  public void setStatus(String status) {
+    this.status = status;
+  }
+  public String getOther() {
+    return other;
+  }
+  public void setOther(String other) {
+    this.other = other;
+  }
+}
